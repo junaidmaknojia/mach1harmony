c0f672854bdb837c1fb41c66a19bf33b6950be40
junaid <junaidmaknojia786@yahoo.com>
2021-03-25 16:55:03 -0500

    finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,5 @@
 const {User} = require("../../db/models");
-const { setTokenCookie } = require("../../utils/auth");
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const asyncHandler = require("express-async-handler");
 const router = require("express").Router();
 
@@ -7,15 +7,25 @@ router.post("/test", function(req,res){
     res.json({requestBody: req.body});
 });
 
-router.get("/set-token-cookie", asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: "demouser"
-        }
-    });
 
-    setTokenCookie(res, user);
-    return res.json({user});
-}));
+//---------USER AUTHENTICATION ROUTER TESTS ------------------
+// router.get("/set-token-cookie", asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: "demouser"
+//         }
+//     });
+
+//     setTokenCookie(res, user);
+//     return res.json({user});
+// }));
+
+// router.get("/restore-user", restoreUser, (req, res) => {
+//     return res.json(req.user);
+// });
+
+// router.get("/require-auth", requireAuth, (req,res) => {
+//     return res.json(req.user);
+// });
 
 module.exports = router;
