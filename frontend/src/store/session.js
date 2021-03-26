cea4ed45748de7f7668b5c5aa8f621e89e61120d
junaid <junaidmaknojia786@yahoo.com>
2021-03-26 01:42:27 -0500

    creating sessionReducer and actions

---
+++ frontend/src/store/session.js
@@ -0,0 +1,19 @@
+const SESSIONADD = "session/add"
+const SESSIONREMOVE = "session/remove"
+
+
+const sessionAdd = () => {{
+    type: SESSIONADD
+}}
+
+const sessionRemove = () => {{
+    type: SESSIONREMOVE
+}}
+
+const sessionReducer = (state=initialState, action) => {
+    let newState = {};
+    // if()
+    switch(action.type){
+
+    }
+}
