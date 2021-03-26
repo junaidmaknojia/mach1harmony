3921d8f44362430dd8a8923353bbea86a50d2869
junaid <junaidmaknojia786@yahoo.com>
2021-03-26 12:20:34 -0500

    created LoginForm index and css and completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,19 +1,46 @@
-const SESSIONADD = "session/add"
-const SESSIONREMOVE = "session/remove"
+import { csrfFetch } from "./csrf";
 
+const SESSION_ADD = "session/setUser"
+const SESSION_REMOVE = "session/removeUser"
+
+const initialState = {user: null};
 
 const sessionAdd = () => {{
-    type: SESSIONADD
+    return {
+        type: SESSION_ADD,
+        payload: user
+    }
 }}
 
 const sessionRemove = () => {{
-    type: SESSIONREMOVE
+    return {type: SESSION_REMOVE}
 }}
+
+export const login = (user) => async (dispatch) => {
+    const {credentials, password} = user;
+    const response = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({ credentials, password })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
 
 const sessionReducer = (state=initialState, action) => {
     let newState = {};
-    // if()
     switch(action.type){
-
+        case SESSION_ADD:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState
+        case SESSION_REMOVE:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
     }
 }
+
+export default sessionReducer;
