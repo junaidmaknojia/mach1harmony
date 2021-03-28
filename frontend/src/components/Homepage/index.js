fb92126d4d65d5c9b9f951a7d351857219eaee93
junaid <junaidmaknojia786@yahoo.com>
2021-03-28 01:37:20 -0500

    called it for today

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -2,7 +2,9 @@ import "./Homepage.css";
 
 export default function Homepage({isLoaded}) {
 
-
+    //have a div for the following section ready
+    // ternery operator for className that determines if you're loggedin
+    // which stuff to show as trending
 
     return (
         <div className="homepage">
