579cd481315603695764f00eddedf2c0f6179a78
junaid <junaidmaknojia786@yahoo.com>
2021-03-28 23:38:18 -0500

    songs router set up

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -2,14 +2,24 @@ const asyncHandler = require("express-async-handler");
 const router = require("express").Router();
 
 
-router.get("/", asyncHandler( async (req,res) => {
+router.get("/:id(\\d+)", asyncHandler( async (req,res) => { // load song page
 
 }));
 
-router.get("/create", asyncHandler( async (req,res) => {
+router.get("/create", asyncHandler( async (req,res) => { // load song upload page
 
 }));
 
-router.post("/create", asyncHandler( async (req,res) => {
+router.post("/create", asyncHandler( async (req,res) => { // handle song upload
 
 }));
+
+router.get("/edit/:id(\\d+)", asyncHandler( async (req,res) => { // load song edit page
+
+}));
+
+router.put("/edit/:id(\\d+)", asyncHandler( async (req,res) => { // handle edits to song
+
+}));
+
+module.exports = router;
