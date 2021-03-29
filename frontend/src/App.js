284146a9d92e31bf170fe1cb9dedcdf70da4abd8
junaid <junaidmaknojia786@yahoo.com>
2021-03-29 02:08:40 -0500

    set up song upload form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormModal";
 import {useDispatch} from "react-redux";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
--- frontend/src/components/UploadSongPage/index.js
+++ frontend/src/components/UploadSongPage/index.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 
 export default function UploadSongPage() {
 
+    const [title, setTitle] = useState();
+    const [artist, setArtist] = useState();
+    const [album, setAlbum] = useState();
+    const [year, setYear] = useState();
+
     return (
         <>
+            <form type="submit" onSubmit={}>
+                <div>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e=>setTitle(e.target.value)}
+                    >
+                    </input>
+                </div>
+                <div>
+                    <input
+                        placeholder="Artist"
+                        value={artist}
+                        onChange={e=>setArtist(e.target.value)}
+                    >
+                    </input>
+                </div>
+                <div>
+                    <input
+                        placeholder="Album"
+                        value={album}
+                        onChange={e=>setAlbum(e.target.value)}
+                    >
+                    </input>
+                </div>
+                <div>
+                    <input
+                        placeholder="Year"
+                        value={year}
+                        onChange={e=>setYear(e.target.value)}
+                    >
+                    </input>
+                </div>
+            </form>
         </>
     );
 }
