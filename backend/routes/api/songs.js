6cd47a8b26e9c8dc2258184ca85fe71300f74e8f
junaid <junaidmaknojia786@yahoo.com>
2021-03-29 17:36:55 -0500

    imported and npm installed multer

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,6 +1,30 @@
 const asyncHandler = require("express-async-handler");
 const router = require("express").Router();
+const express = require("express");
+const multer = require('multer');
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+            cb(null, true)
+        }
+        else {
+            cb(null, false)
+            return cb(new Error('Only images files valid'))
+        }
+    }
+}).single('imageupload');
 
 router.get("/:id(\\d+)", asyncHandler( async (req,res) => { // load song page
 
