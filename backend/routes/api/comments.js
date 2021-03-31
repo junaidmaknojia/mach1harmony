8ad17f0d837bfce73d816b29acfe8f499ca9f50f
junaid <junaidmaknojia786@yahoo.com>
2021-03-31 03:05:57 -0500

    comments add and get apis done

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
 const asyncHandler = require("express-async-handler");
+const { Comment, User } = require('../../db/models');
 
-router.post("/create", asyncHandler(async (req, res) => { // create a comment on a song
+
+router.post("/:id", asyncHandler(async (req, res) => { // create a comment on a song
+    const songOnComment = req.params.id;
+    const { comment, userId, songId } = req.body;
+
+    const createdComment = await Comment.create({ text: comment, userId, songId });
+    return res.json({ createdComment });
+
+}));
+
+router.get("/:id", asyncHandler(async (req, res) => { // get all the comments on a song
+    const songId = req.params.id;
+
+    const comments = await Comment.findAll({
+        include: [{
+            model: User
+        }],
+        where: { songId },
+        order: [[
+            'createdAt', 'DESC'
+        ]]
+    });
+
+    return res.json(comments);
 
 }));
 
