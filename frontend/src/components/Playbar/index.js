b1c8140bceb322f7d9fa9b87f5cf5e426c61474b
junaid <junaidmaknojia786@yahoo.com>
2021-04-01 01:08:33 -0500

    click icon adds controller to bar but manual play, demouser profile page link stops it

--- frontend/src/components/Playbar/index.js
+++ frontend/src/components/Playbar/index.js
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux";
 import "./Playbar.css";
 
 export default function Playbar(){
 
-
+const currSong = useSelector(state => state.song.currSong);
 
     return (
         <div className="barContainer">
-
+            {currSong && (
+               <>
+                    <audio controls>
+                        <source src={currSong.filePath} type="audio/mpeg" autoplay/>
+                    </audio>
+               </>
+            )}
         </div>
     );
 }
