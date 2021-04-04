8deef287a8e533f018a5cbb18ee9c03b0d654438
junaid <junaidmaknojia786@yahoo.com>
2021-04-04 15:58:27 -0500

    playbar good enough, no scroll or volume yet

--- frontend/src/components/Playbar/index.js
+++ frontend/src/components/Playbar/index.js
@@ -30,15 +30,40 @@ export default function Playbar(){
         }
     }
 
+    // <ReactSlider
+    // 			value={thermometer}
+    // 			onAfterChange={(val) => {
+    // 				setThermometer(val);
+    // 			}}
+    // 			className="thermometer-slider"
+    // 			thumbClassName="thermometer-thumb"
+    // 			trackClassName="thermometer-track"
+    // 			ariaLabel={"Thermometer"}
+    // 			orientation="vertical"
+    // 			min={0}
+    // 			max={120}
+    // 			renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+    // 			renderTrack={(props, state) => <div {...props} index={state.index}></div>}
+    // 			invert
+    // 			pearling
+    // 			minDistance={1}
+    // 		/>
+
     return (
         <div className="barContainer">
             {addedSong && (
-                <div >
-                    {audioRef.current && (
-                        playing ? <img className="playPause" src="https://www.freeiconspng.com/thumbs/pause-button-png/pause-button-png-32.png" onClick={handlePlay}></img> : <img className="playPause" src="https://www.freeiconspng.com/uploads/pause-button-png-31.png" onClick={handlePlay}></img>
-                    )}
-                    <audio src={addedSong.filePath} ref={audioRef} className="audioControls"></audio>
-                </div>
+                <>
+                    <span><img style={{width: 40}} src={addedSong.coverPhoto}/></span>
+                    <span style={{fontSize: 20, fontWeight: "bold"}}>{addedSong.title}</span>
+                    <span>{addedSong.album}</span>
+                    <span style={{width: 40, border: "black"}}>
+                        {audioRef.current && (
+                            playing ? <img className="playPause" src="https://www.freeiconspng.com/thumbs/pause-button-png/pause-button-png-32.png" onClick={handlePlay}></img> : <img className="playPause" src="https://www.freeiconspng.com/uploads/play-button-icon-png-0.png" onClick={handlePlay}></img>
+                            )}
+
+                        <audio src={addedSong.filePath} ref={audioRef} className="audioControls"></audio>
+                    </span>
+                </>
             )}
         </div>
     );
