65afbc7912cd9c4d23d773a6fda2f5b0f0b651ae
junaid <junaidmaknojia786@yahoo.com>
2021-04-06 23:26:39 -0500

    set up genre list part of component

--- frontend/src/components/GenresPage/index.js
+++ frontend/src/components/GenresPage/index.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
 import "./GenresPage.css";
 
 export default function GenresPage() {
+    const [genreSelected, setGenreSelected] = useState([]);
 
+    function showSongs(e){
+        // console.log(e.target.value);
+        // grab the songs
+        setGenreSelected(genreSongs);
+    }
+
+    return (
+        <>
+            <div className="genreOptions">
+                {/* {genres && (
+                    genres.map(genre => {
+                        return (
+                            <p
+                            onClick={showSongs}
+                            value={genre.id}
+                            className="genreOptions__genre">{genre.name}</p>
+                        )
+                    })
+                )} */}
+            </div>
+            <div className="genreSongs">
+                {/* {genreSelected && (
+                    genreSelected.map(song => {
+                        return (
+                            <div>
+                                <img src={song.coverPhoto}/>
+                                <span>{song.title}</span>
+                                <span>{song.artist}</span>
+                                <span>{song.album}</span>
+                                <span>{song.numListens}</span>
+                            </div>
+                        )
+                    })
+                )} */}
+            </div>
+        </>
+    )
 }
