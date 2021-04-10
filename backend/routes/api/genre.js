ff6a560d32a4d97f7633d4d017e619477b16fc5c
junaid <junaidmaknojia786@yahoo.com>
2021-04-09 22:59:57 -0500

    genre router is done to test

--- backend/routes/api/genre.js
+++ backend/routes/api/genre.js
@@ -5,3 +5,18 @@ const asyncHandler = require("express-async-handler");
 
 
 module.exports = router;
+
+router.get("/genres", asyncHandler(async (req,res) => {
+    const genres = await Genre.findAll();
+
+    return res.json(genres)
+}));
+
+router.get("/genres/:id", asyncHandler(async (req,res) => {
+    const genreId = req.params.id;
+    const songs = await Song.findAll({
+        where: genreId
+    });
+
+    return res.json(songs)
+}));
