97c59aed44f17d991788d62f96fbb1600e103f15
junaid <junaidmaknojia786@yahoo.com>
2021-04-09 21:53:40 -0500

    created genre router in backend

---
+++ backend/routes/api/genre.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { User, Song, Follow } = require('../../db/models');
+const asyncHandler = require("express-async-handler");
+
+
+
+module.exports = router;
