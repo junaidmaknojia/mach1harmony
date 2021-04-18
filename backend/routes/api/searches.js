86e44640b5eba837eda97bd235cb66df9f698795
junaid <junaidmaknojia786@yahoo.com>
2021-04-18 02:06:46 -0500

    search is not being fetched

--- backend/routes/api/searches.js
+++ backend/routes/api/searches.js
@@ -2,28 +2,24 @@ const router = require("express").Router();
 const { User, Song, Follow, Genre } = require('../../db/models');
 const asyncHandler = require("express-async-handler");
 
-router.get("/search/:search", asyncHandler(async (req,res) => {
+router.get("/:search", asyncHandler(async (req,res) => {
     const search = req.params.search;
 
-    // const songs = await Song.findAll({
-    //     where: {
-    //         userId: {
-    //             // [Op.<operator>]: <value>
-    //         }
-    //     },
-    //     {/* include: <include_specifier>, */}
-    //     offset: 10,
-    //     limit: 2
-    // });
     const songs = await Song.findAll();
     const users = await User.findAll();
 
     const foundSongs = songs.filter(song => {
         const catches = [song.title.toLowerCase(), song.album.toLowerCase(), song.artist.toLowerCase()];
-        return catches.includes(search.toLowerCase())
+        return catches.includes(search.toLowerCase());
     });
 
-    // return res.json(genres)
+    const foundUsers = users.filter(user => {
+        const name = user.username.toLowerCase();
+        return name.includes(search.toLowerCase());
+    });
+
+    return res.json({foundSongs, foundUsers});
+
 }));
 
 
