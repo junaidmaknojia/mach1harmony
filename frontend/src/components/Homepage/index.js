143e69b2191e20b55bfa015cce00b6e5b1cde3a2
junaid <junaidmaknojia786@yahoo.com>
2021-05-31 01:54:20 -0500

    calling it for Sunday

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -47,34 +47,32 @@ export default function Homepage({isLoaded}) {
 
     return (
         <div className="homepage">
-            <div className={`banner ${following}`}>
-                <Carousel className="carousel">
-                    <Carousel.Item>
-                        <div className="carousel__main carouselText">
-                            <h2>Discover more with this SoundCloud clone</h2>
-                            <p>Mach1Harmony allows you to get the same experience of SoundCloud, but on a whole other site. Listen solely on your desktop and connect with the limited people that know about the application!</p>
-                            <SignupFormModal text="Get Started"/>
-                        </div>
-                        <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/sunset-people.jpg" className="sliderimg d-block w-100"/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <div className="carousel__second carouselText">
-                            <h2>Upload your hits</h2>
-                            <p>With over 20 users (yeah... that's right), your tracks will gain all kinds of traction with Mach1Harmony. Start uploading your tracks for mid-size virality!</p>
-                            <SignupFormModal text="Upload Now"/>
-                        </div>
-                        <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/piano-night-club.jpg" className="sliderimg d-block w-100"/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <div className="carousel__third carouselText">
-                            <h2>{"Ad partner goes \<here\>"}</h2>
-                            <p>That somone in the crowd will grant this application's big break and will be featured here as an ad partner. Conditions will follow</p>
-                        </div>
-                        <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/night-dj.jpg" className="sliderimg d-block w-100"/>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-            <div className={`trending ${following}`}>
+            <Carousel className="carousel">
+                <Carousel.Item>
+                    <div className="carousel__main carouselText">
+                        <h2>Discover more with this SoundCloud clone</h2>
+                        <p>Mach1Harmony allows you to get the same experience of SoundCloud, but on a whole other site. Listen solely on your desktop and connect with the limited people that know about the application!</p>
+                        <SignupFormModal text="Get Started"/>
+                    </div>
+                    <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/sunset-people.jpg" className="sliderimg d-block w-100"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="carousel__second carouselText">
+                        <h2>Upload your hits</h2>
+                        <p>With over 20 users (yeah... that's right), your tracks will gain all kinds of traction with Mach1Harmony. Start uploading your tracks for mid-size virality!</p>
+                        <SignupFormModal text="Upload Now"/>
+                    </div>
+                    <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/piano-night-club.jpg" className="sliderimg d-block w-100"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="carousel__third carouselText">
+                        <h2>{"Ad partner goes \<here\>"}</h2>
+                        <p>That somone in the crowd will grant this application's big break and will be featured here as an ad partner. Conditions will follow</p>
+                    </div>
+                    <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/night-dj.jpg" className="sliderimg d-block w-100"/>
+                </Carousel.Item>
+            </Carousel>
+            <div className="trending">
                 <h2>Trending</h2>
                 <div>
                     {loadSongs && (
@@ -92,7 +90,7 @@ export default function Homepage({isLoaded}) {
                     )}
                 </div>
             </div>
-            <div className={`explore ${following}`}>
+            <div className="explore">
                 <h2>Explore</h2>
                 <div>
                     {loadUsers && (
@@ -115,7 +113,7 @@ export default function Homepage({isLoaded}) {
                 <SignupFormModal text="Sign Up"/>
                 <img src="https://react-project.s3.us-east-2.amazonaws.com/stock/shelf-headphones.jpg"/>
             </div> */}
-            <div className={`getStarted ${following}`}></div>
+            <div className="getStarted"></div>
             {sessionUser && (
                 <div className="followingDiv">
                     <h3 style={{marginLeft: 25, marginRight: 25, textAlign: "center"}}>People You're Following</h3>
