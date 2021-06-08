caeafd07767716fe2e0cd049d429ce332d8935fe
junaid <junaidmaknojia786@yahoo.com>
2021-06-07 19:06:28 -0500

    footer ready to attach everywhere

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer(){
@@ -5,9 +6,11 @@ export default function Footer(){
 
     return (
         <div className="footer">
+            <Link to="/" className="footer__home">Home</Link>
+            <p>Mach1Harmony claims no rights to the demo tracks. They are used for demonstration purposes only</p>
             <a href="https://github.com/junaidmaknojia/" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
             <a href="https://www.linkedin.com/in/junaidmaknojia/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><i class="fas fa-briefcase"></i></a>
+            <a href="https://junaidmaknojia.github.io/" target="_blank" rel="noopener noreferrer"><i class="fas fa-briefcase"></i></a>
         </div>
     )
 }
